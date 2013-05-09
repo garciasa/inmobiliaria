563aa1655e5b2d9224eec82f0ef337e396ffb774
nio <garciasa@gmail.com>
2013-05-09 13:28:27 +0200

    parametro para que haga el rebuild del solr solo

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -159,6 +159,9 @@ HAYSTACK_CONNECTIONS = {
 
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 3
 
+#Para que automaticamente rebuild, cuando haya un cambio
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
